2dea5c068538c964795eb5a4ab729f9884666ff0
Abdul-Rahim Adada <abdulrahim88@gmail.com>
2023-03-31 23:38:09 +0000

    store application in database

--- database.py
+++ database.py
@@ -31,5 +31,10 @@ def load_job_from_db(id):
     else:
       return dict(rows[0])
 
+def add_application_to_db(job_id, data):
+  with engine.connect() as con:
+    query = text('INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES(:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)')
+    con.execute(query, {'job_id': job_id, 'full_name': data['full_name'], 'email': data['email'], 'linkedin_url': data['linkedin_url'], 'education': data['education'], 'work_experience': data['work_experience'], 'resume_url': data['resume_url']})
+
   
     
